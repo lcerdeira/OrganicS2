7c9fbf0a8b43ba5cdc355bafb84813435add5fd8
Louise Teixeira Cerdeira <lcerdeira@gmail.com>
2020-08-12 21:06:27 +1000

    draft

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# p2idea
+#Temporary_name-APP
+> ### Example of using MYSQL with an ORM on an Expressjs server using Nodejs with the MVC model. This application performes all CRUD operations to a MYSQL database. 
+
+## Project Overview
+
+This app allows you ......
+
+# View Demo  
+
+To view demo click on this link: ([Heroku Demo Link]())
+
+# Getting Started Locally 
+
+To get the Node server running locally:
+
+- Clone this repo ([GIT](https://github.com/lcerdeira/p2idea))
+- Install Nodejs on your Mac ([Download](https://github.com/lcerdeira/p2idea))
+- `npm install` to install all required dependencies
+- Check credentials the .sql file locally and make sure the credentials match up. 
+- `node server.js` to start the node application.
+
+
+# Code Overview
+
+## Dependencies
+- [body-parser](https://www.npmjs.com/package/body-parser) - Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+- [express](https://www.npmjs.com/package/express) - Fast, unopinionated, minimalist web framework (for servers) for node.
+- [path](https://www.npmjs.com/package/path) - This is an exact copy of the NodeJS ’path’ module published to the NPM registry.
+- [dotenv](https://www.npmjs.com/package/dotenv) - Loads environment variables from .env file
+- [handlebars](https://www.npmjs.com/package/handlebars) - andlebars provides the power necessary to let you build semantic templates effectively with no frustration
+- [mysql](https://www.npmjs.com/package/mysql) - A node.js driver for mysql. It is written in JavaScript
+
+## Dev Dependencies
+
+- [nodmon](https://www.npmjs.com/package/nodemon) - nodemon will watch the files in the directory in which nodemon was started, and if any files change, nodemon will automatically restart your node application.
+
+## Application MVC Structure
+
+- `server.js` - The entry point to our application for the customer user type.
+- `routes.js` - Entry point for the applications routes.
+- `/views` - Applications template files using handlebars.
+- `/routes` - The routes of this application.
+- `/public` - All public facing assets are in this directory.
+- `/models` - The application models for use with ORM.
+- `/db` - Starter database files.
+- `/controllers` - Most of the application's logic for requests and responses are done in this directory. 
+- `/config` - Database configuration. 
--- server.js
+++ server.js
@@ -1,36 +1,51 @@
-// Requiring necessary npm packages
-const express = require("express");
-const session = require("express-session");
-// Requiring passport as we've configured it
-const passport = require("./config/passport");
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
 
-// Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 8080;
-const db = require("./models");
+// Dependencies
+// =============================================================
+const EXPRESS = require("express");
+const BODYPARSER = require("body-parser");
+const PATH = require("path");
+const EXPHBS = require("express-handlebars");
 
-// Creating express app and configuring middleware needed for authentication
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
-// We need to use sessions to keep track of our user's login status
-app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+// Sets up the Express App
+// =============================================================
+const APP = EXPRESS();
+const PORT = process.env.PORT || 9000;
 
-// Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+// Sets up the Express app to handle data parsing
 
-// Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// parse application/x-www-form-urlencoded
+APP.use(BODYPARSER.urlencoded({ extended: true }));
+// parse application/json
+APP.use(BODYPARSER.json());
+
+// Static directory
+APP.use(EXPRESS.static(PATH.join(__dirname, 'public')));
+
+// Routes
+// =============================================================
+require('./routes')(APP);
+// Import routes and give the server access to them.
+
+// catch 404 and forward to error handler
+APP.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// Views
+// ============================================================
+APP.engine("handlebars", EXPHBS({ defaultLayout: "main" }));
+APP.set("view engine", "handlebars");
+
+// Starts the server to begin listening
+// =============================================================
+APP.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+// our module get's exported as app.
+module.exports = APP;
