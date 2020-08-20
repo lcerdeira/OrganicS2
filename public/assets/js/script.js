348d66625c3e10d38c5229e751ceea45a244e172
bdcoelho <ben_coelho@hotmail.com>
2020-08-20 23:27:29 +1000

    Minor bug fix - button target

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -11,18 +11,21 @@ $(document).ready(() => {
     console.log("clicked");
     event.preventDefault();
     const target = $(event.target);
-    const shoppingListItem = {
-      itemId: target
-        .parent()
-        .parent()
-        .attr("id"),
-      itemName: $(target.parent().siblings()[0]).text(),
-      itemPrice: $(target.parent().siblings()[1]).text(),
-      itemQty: 3,
-      itemUnit: $(target.parent().siblings()[2]).text(),
-    };
-    storageArray.push(shoppingListItem);
-    console.log(storageArray);
-    localStorage.setItem("shoppingCart", JSON.stringify(storageArray));
+
+    if (target.is("button")) {
+      const shoppingListItem = {
+        itemId: target
+          .parent()
+          .parent()
+          .attr("id"),
+        itemName: $(target.parent().siblings()[0]).text(),
+        itemPrice: $(target.parent().siblings()[1]).text(),
+        itemQty: 3,
+        itemUnit: $(target.parent().siblings()[2]).text(),
+      };
+      storageArray.push(shoppingListItem);
+      console.log(storageArray);
+      localStorage.setItem("shoppingCart", JSON.stringify(storageArray));
+    }
   });
 });
